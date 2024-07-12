96f2b7cd48a97686024bcb52a520145a483688bf
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-12 12:28:13 +1000

    camera working

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -4,8 +4,20 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
 while True:
     success, img = cap.read()
 
+    if not success:
+        print("Failed to capture image")
+        break
+
     cv2.imshow("Image", img)
-    cv2.waitKey(0)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
