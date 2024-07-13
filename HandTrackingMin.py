6df93bc1dcea8a88357e11912561d9d6ffb54723
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-13 15:31:07 +1000

    created function to draw hand

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
-
+mpDraw = mp.solutions.drawing_utils
 
 if not cap.isOpened():
     print("Error: Could not open camera.")
@@ -17,7 +17,10 @@ while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    print(results.multi_hand_landmarks)
+    # print(results.multi_hand_landmarks)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms)
 
     if not success:
         print("Failed to capture image")
