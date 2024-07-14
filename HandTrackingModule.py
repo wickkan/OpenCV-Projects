87fab561edfd23ca567dbff45ca730bc57ecc7c0
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-14 15:38:14 +1000

    hand tracking module working

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -13,38 +13,33 @@ class handDetector():
 
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(
-            self.mode, self.maxHands, self.detectionCon, self.trackCon)
+            static_image_mode=self.mode,
+            max_num_hands=self.maxHands,
+            min_detection_confidence=self.detectionCon,
+            min_tracking_confidence=self.trackCon
+        )
         self.mpDraw = mp.solutions.drawing_utils
 
-    def findHands(self, img):
+    def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
         results = self.hands.process(imgRGB)
+
         if results.multi_hand_landmarks:
             for handLms in results.multi_hand_landmarks:
-                for id, lm in enumerate(handLms.landmark):
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    if id == 4:  # tracking the tip of the thumb
-                        cv2.circle(img, (cx, cy), 15,
-                                   (255, 0, 255), cv2.FILLED)
-
-                self.mpDraw.draw_landmarks(
-                    img, handLms, self.mpHands.HAND_CONNECTIONS)
+                if draw:
+                    self.mpDraw.draw_landmarks(
+                        img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
-
-
-cap.release()
-cv2.destroyAllWindows()
 
 
 def main():
     pTime = 0
     cTime = 0
     cap = cv2.VideoCapture(0)
-
+    detector = handDetector()
     while True:
         success, img = cap.read()
+        img = detector.findHands(img)
 
         cTime = time.time()
         fps = 1/(cTime-pTime)
