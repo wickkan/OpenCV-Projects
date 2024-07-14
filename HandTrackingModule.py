513672e8ad22d8e1ed578d08abb0a6b7d7c27144
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-14 15:48:30 +1000

    tracking successfully done in the module

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -19,17 +19,31 @@ class handDetector():
             min_tracking_confidence=self.trackCon
         )
         self.mpDraw = mp.solutions.drawing_utils
+        self.results = None
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(imgRGB)
+        self.results = self.hands.process(imgRGB)
 
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(
                         img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+        lmList = []
+        if self.results and self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        return lmList
 
 
 def main():
@@ -39,18 +53,19 @@ def main():
     detector = handDetector()
     while True:
         success, img = cap.read()
+        if not success:
+            print("Failed to capture image")
+            break
+
         img = detector.findHands(img)
+        lmList = detector.findPosition(img)
 
         cTime = time.time()
-        fps = 1/(cTime-pTime)
+        fps = 1 / (cTime - pTime)
         pTime = cTime
 
         cv2.putText(img, str(int(fps)), (10, 70),
                     cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
-        if not success:
-            print("Failed to capture image")
-            break
 
         cv2.imshow("Image", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
