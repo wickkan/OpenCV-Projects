91ad54e93644022dd1527e5de7dbd95cf612279a
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-14 14:31:43 +1000

    fixed findHands

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -16,20 +16,22 @@ class handDetector():
             self.mode, self.maxHands, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
+    def findHands(self, img):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-results = hands.process(imgRGB)
- # print(results.multi_hand_landmarks)
+        results = self.hands.process(imgRGB)
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                for id, lm in enumerate(handLms.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    if id == 4:  # tracking the tip of the thumb
+                        cv2.circle(img, (cx, cy), 15,
+                                   (255, 0, 255), cv2.FILLED)
 
- if results.multi_hand_landmarks:
-      for handLms in results.multi_hand_landmarks:
-           for id, lm in enumerate(handLms.landmark):
-                # print(id, lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x*w), int(lm.y*h)
-                if id == 4:  # tracking the tip of the thumb
-                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+                self.mpDraw.draw_landmarks(
+                    img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
 
 
 cap.release()
