8898551422269b02b8b02870195ce6dd919fb006
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-15 10:42:41 +1000

    added FPS tracker

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -9,8 +9,16 @@ wCam, hCam = 1280, 720
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
+pTime = 0
 while True:
     success, img = cap.read()
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 70),
+                cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 255), 3)
 
     cv2.imshow("Img", img)
     cv2.waitKey(1)
