d5cef80dc5eba1ae7ba4278dd64ffd66a524245f
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-16 11:11:09 +1000

    added a shortcut when pressing q to quit the window and break the loop

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -14,24 +14,36 @@ pTime = 0
 
 detector = htm.handDetector(detectionCon=0.7)
 
-
 while True:
     success, img = cap.read()
+    if not success:
+        print("Failed to capture image")
+        break
+
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
         x1, y1 = lmList[4][1], lmList[4][2]
         x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
         cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
         cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
         cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
     cTime = time.time()
-    fps = 1/(cTime-pTime)
+    fps = 1 / (cTime - pTime)
     pTime = cTime
 
     cv2.putText(img, f'FPS: {int(fps)}', (40, 50),
                 cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 3)
 
     cv2.imshow("Img", img)
-    cv2.waitKey(1)
+
+    # Check for 'q' key press to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
