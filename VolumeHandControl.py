2b052a8f4f26f85f6dc27fea0bf5af34d8c760ae
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-16 11:00:05 +1000

    adjusted fps

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -17,8 +17,8 @@ while True:
     fps = 1/(cTime-pTime)
     pTime = cTime
 
-    cv2.putText(img, f'FPS: {int(fps)}', (40, 70),
-                cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 255), 3)
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50),
+                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 3)
 
     cv2.imshow("Img", img)
     cv2.waitKey(1)
