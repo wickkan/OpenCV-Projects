7d3d8aeff405df03a60f7a03096df824fe8e2835
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-16 11:07:30 +1000

    added a circle to track index and thumb

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -19,6 +19,11 @@ while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
