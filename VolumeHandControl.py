67483778833840ca284e17efb96ef309f15188a6
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-16 11:03:17 +1000

    added hand tracking module to volume hand control file

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import numpy as np
+import HandTrackingModule as htm
 
 ######################
 wCam, hCam = 1280, 720
@@ -10,8 +11,13 @@ cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
+
+detector = htm.handDetector()
+
+
 while True:
     success, img = cap.read()
+    img = detector.findHands(img)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
