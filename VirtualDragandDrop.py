993c54be621d135144a75ed96707d974b2eed61c
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-18 12:44:58 +1000

    added hand tracking and drawing

--- VirtualDragandDrop.py
+++ VirtualDragandDrop.py
@@ -1,11 +1,16 @@
 import cv2
+from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
+detector = HandDetector(detectionCon=0.8)
+
 
 while True:
     success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, _ = detector.findPosition(img)
     if not success:
         print("Failed to capture image")
         break
