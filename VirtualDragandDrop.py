f4d17199ed2827be01c540a5cc7ea86acb45e7fb
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-19 12:17:31 +1000

    added a rectangle around hand and fixed colour changing issue

--- VirtualDragandDrop.py
+++ VirtualDragandDrop.py
@@ -1,12 +1,10 @@
 import cv2
-import HandTrackingModule as htm
+from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
-
-# Initialise the hand detector
-detector = htm.handDetector(detectionCon=0.7)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 colourR = (255, 0, 255)
 
 while True:
@@ -15,17 +13,17 @@ while True:
         print("Failed to capture image")
         break
     img = cv2.flip(img, 1)
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img)
-    if lmList:
-        cursor = lmList[8]
+    hands, img = detector.findHands(img)  # Updated method call
+
+    if hands:
+        lmList = hands[0]['lmList']  # Get the first hand detected
+        cursor = lmList[8]  # Index finger tip
         if 100 < cursor[0] < 300 and 100 < cursor[1] < 300:
-            colourR = 0, 255, 0
+            colourR = (0, 255, 0)
         else:
             colourR = (255, 0, 255)
 
     cv2.rectangle(img, (100, 100), (300, 300), colourR, cv2.FILLED)
-
     cv2.imshow("Image", img)
 
     # Check for 'q' key press to exit
