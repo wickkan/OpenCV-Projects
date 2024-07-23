5d078488fe5bc738429ca406646e05e9a1a69aea
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-23 12:48:31 +1000

    find hands

--- HandDistance.py
+++ HandDistance.py
@@ -6,9 +6,12 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
+# Hand Detector
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
 # loop
 while True:
     success, img = cap.read()
-
+    hands, img = detector.findHands(img)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
