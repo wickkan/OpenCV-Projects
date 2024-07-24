a8b39b02ec75012a4e37ceef366a097d6c35ca50
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-24 11:26:22 +1000

    added find function

--- HandDistance.py
+++ HandDistance.py
@@ -10,6 +10,13 @@ cap.set(4, 720)   # Height
 # Hand Detector
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 
+
+# Find Function
+# x is the raw distance, y is the value in cm
+x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
+y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+
+
 # Loop
 while True:
     success, img = cap.read()
@@ -23,7 +30,7 @@ while True:
         x1, y1 = lmList[5]
         x2, y2 = lmList[17]
 
-        distance = math.sqrt((y2-y1)**2 + (x2-x1)**2)
+        distance = int(math.sqrt((y2-y1)**2 + (x2-x1)**2))
 
     cv2.imshow("Image", img)
 
