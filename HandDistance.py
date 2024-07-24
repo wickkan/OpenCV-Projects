85541e7503f520393049325866c1dc6ed27392e7
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-24 11:34:28 +1000

    fixed hand measurement issue

--- HandDistance.py
+++ HandDistance.py
@@ -12,13 +12,11 @@ cap.set(4, 720)   # Height
 # Hand Detector
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 
-
 # Find Function
 # x is the raw distance, y is the value in cm
 x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
 y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
 coff = np.polyfit(x, y, 2)  # y = Ax^2 + Bx + C
-
 
 # Loop
 while True:
@@ -26,19 +24,21 @@ while True:
     if not success:
         print("Failed to capture image")
         break
-    hands, img = detector.findHands(img)
+
+    # Unpack the tuple returned by findHands
+    hands, img = detector.findHands(img, draw=False)
 
     if hands:
         lmList = hands[0]['lmList']
-        bbox = hands[0]['bbox']
-        x1, y1 = lmList[5]
-        x2, y2 = lmList[17]
+        x, y, w, h = hands[0]['bbox']
+        x1, y1 = lmList[5][:2]  # Get the coordinates of landmark 5
+        x2, y2 = lmList[17][:2]  # Get the coordinates of landmark 17
 
-        distance = int(math.sqrt((y2-y1)**2 + (x2-x1)**2))
+        distance = int(math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2))
         A, B, C = coff
-        distanceCM = A*distance**2 + B*distance + C
+        distanceCM = A * distance ** 2 + B * distance + C
 
-        cvzone.putTextRect(img, f'{int(distanceCM)} cm', )
+        cvzone.putTextRect(img, f'{int(distanceCM)} cm', (x, y))
 
     cv2.imshow("Image", img)
 
