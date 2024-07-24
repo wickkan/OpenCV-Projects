27664019667283e0f2f87aba86b707abd0f610d6
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-24 11:29:34 +1000

    added distance in hands

--- HandDistance.py
+++ HandDistance.py
@@ -1,6 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import math
+import numpy as np
 
 # Webcam setup
 cap = cv2.VideoCapture(0)
@@ -15,6 +16,7 @@ detector = HandDetector(detectionCon=0.8, maxHands=1)
 # x is the raw distance, y is the value in cm
 x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
 y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+coff = np.polyfit(x, y, 2)  # y = Ax^2 + Bx + C
 
 
 # Loop
@@ -31,6 +33,8 @@ while True:
         x2, y2 = lmList[17]
 
         distance = int(math.sqrt((y2-y1)**2 + (x2-x1)**2))
+        A, B, C = coff
+        distanceCM = A*distance**2 + B*distance + C
 
     cv2.imshow("Image", img)
 
