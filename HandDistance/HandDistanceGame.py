93debb2f9b2fb476e1d5b8970fa62cd348bc5e95
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-26 20:23:16 +1000

    started editing the game segment

--- HandDistance/HandDistanceGame.py
+++ HandDistance/HandDistanceGame.py
@@ -42,6 +42,9 @@ while True:
         A, B, C = coff
         distanceCM = A * distance ** 2 + B * distance + C
 
+        if distanceCM < 40:
+            color = (0, 255, 0)
+
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 255), 3)
         cvzone.putTextRect(img, f'{int(distanceCM)} cm', (x+5, y-10))
 
