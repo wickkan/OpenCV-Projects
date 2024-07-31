b50aa05fa946a8faf0554156e493e129aabdda23
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-07-31 18:02:10 +1000

    added the q exit button

--- ParkingSpaceCounter/ParkingSpacePicker.py
+++ ParkingSpaceCounter/ParkingSpacePicker.py
@@ -2,8 +2,7 @@ import cv2
 import pickle
 
 # Load the image
-img = cv2.imread(
-    'ParkingSpaceCounter/carParking.png')
+img = cv2.imread('ParkingSpaceCounter/carParking.png')
 
 # Check if the image was loaded successfully
 if img is None:
@@ -13,7 +12,10 @@ if img is None:
 while True:
     cv2.rectangle(img, (80, 230), (200, 150), (255, 0, 255), 2)
     cv2.imshow("image", img)
+
+    # Check for 'q' key press to exit the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Release resources and close windows
 cv2.destroyAllWindows()
