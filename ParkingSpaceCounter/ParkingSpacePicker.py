53498714eed2bf824dda523e14e1fba0dd15ae9c
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-08-07 14:30:57 +1000

    fixing delete rectangle error

--- ParkingSpaceCounter/ParkingSpacePicker.py
+++ ParkingSpaceCounter/ParkingSpacePicker.py
@@ -16,6 +16,11 @@ posList = []
 def mouseClick(events, x, y, flags, params):
     if events == cv2.EVENT_LBUTTONDOWN:
         posList.append((x, y))
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(posList):
+            x1, y1 = pos
+            if x1 < x < x1+width and y1 < y < y1 + height:
+                posList.pop(i)
 
 
 cv2.namedWindow("image")
