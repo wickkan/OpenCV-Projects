7a82c2a9a2fa9da59e5ef1c12f06f0ffbaf0a72d
Kanishka <kanishkawickramasinghe50@gmail.com>
2024-08-08 16:52:25 +1000

    main code setup

--- ParkingSpaceCounter/main.py
+++ ParkingSpaceCounter/main.py
@@ -1,3 +1,12 @@
 import cv2
 import pickle
 import cvzone
+import numpy as np
+
+# video feed
+cap = cv2.VideoCapture('carPark.mp4')
+
+while True:
+    success, img = cap.read()
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)
